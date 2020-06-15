696e84d074f240b415ef3bfb6bc39fa9be497760
Thorsten Bux <thorsten.bux@outlook.com>
2020-06-15 15:20:22 +1200

    play video

--- nftCreatorWS/docs/test.js
+++ nftCreatorWS/docs/test.js
@@ -1,4 +1,4 @@
 function start() {
   const nftLink = document.getElementById('nftLink').value
-  window.location.href = "https://hungry-perlman-314f0a.netlify.app/?trackable=" + nftLink
+  window.location.href = "https://demo1.tripod-digital.co.nz/?trackable=" + nftLink + "&video=https://cafe.tripod-digital.co.nz/videos/surf.mp4"
 }
