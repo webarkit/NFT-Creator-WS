feb44f0244bd8f30a1af08b64927f3b67bf4e0ec
Thorsten Bux <thorsten.bux@outlook.com>
2020-06-15 17:28:16 +1200

    change intro video

--- nftCreatorWS/docs/test.js
+++ nftCreatorWS/docs/test.js
@@ -1,4 +1,4 @@
 function start() {
   const nftLink = document.getElementById('nftLink').value
-  window.location.href = "https://demo1.tripod-digital.co.nz/?t=" + btoa(nftLink) + "&v="+ btoa("https://cafe.tripod-digital.co.nz/videos/surf.mp4")
+  window.location.href = "https://demo1.tripod-digital.co.nz/?t=" + btoa(nftLink) + "&v="+ btoa("https://cafe.tripod-digital.co.nz/videos/webAR_NFT.mp4")
 }
